c1c31a81de193ac46cc6d160dfe11197240a4419
ygk313 <ygk313@naver.com>
2023-03-27 21:57:30 +0900

    delete comma

--- lohbs_pick/settings.py
+++ lohbs_pick/settings.py
@@ -91,7 +91,7 @@ WSGI_APPLICATION = 'lohbs_pick.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
     }
 }
 
