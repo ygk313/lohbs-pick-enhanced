77dd002057307056cc72124323c666d5b8cf9c5e
ygk313 <ygk313@naver.com>
2023-04-17 21:55:33 +0900

    Adopt django-dotenv

--- lohbs_pick/settings.py
+++ lohbs_pick/settings.py
@@ -22,7 +22,7 @@ credentials = json.loads(open(CREDENTIAL_FILE).read())
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = credentials["SECRET_KEY"]
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
